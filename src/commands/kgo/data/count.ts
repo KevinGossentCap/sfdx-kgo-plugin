ee038c5cb58f52e9774c99443095322f8bde3b94
GOSSENT <kevin.gossent@capgemini.com>
2021-02-09 14:19:43 +0100

    move recordCount result output to standard sfdx features

--- src/commands/kgo/data/count.ts
+++ src/commands/kgo/data/count.ts
@@ -1,15 +1,8 @@
-import {SfdxCommand} from '@salesforce/command'
+import {SfdxCommand, SfdxResult} from '@salesforce/command'
 // import {flags, SfdxCommand} from '@salesforce/command'
 // import {Messages, SfdxError} from '@salesforce/core';
 import {AnyJson} from '@salesforce/ts-types'
 import * as _ from 'lodash'
-
-const formatTabColomn = {
-  columns: [
-    {key: 'name', label: 'Name'},
-    {key: 'count', label: 'Count'}
-  ]
-}
 
 export default class KgoDataCount extends SfdxCommand {
   static description = 'retrieves record counts from REST API recordCount'
@@ -23,16 +16,6 @@ export default class KgoDataCount extends SfdxCommand {
   // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
   protected static requiresProject = false
 
-  // static flags = {
-  //   help: flags.help({char: 'h'}),
-  //   // flag with a value (-n, --name=VALUE)
-  //   name: flags.string({char: 'n', description: 'name to print'}),
-  //   // flag with no value (-f, --force)
-  //   force: flags.boolean({char: 'f'}),
-  // }
-
-  // static args = [{name: 'file'}]
-
   treatResultType(result, strType: string, objectsToCount: string[], objectRecordMap) {
     let tmpArray = []
     for (let elem of objectsToCount) {
@@ -40,10 +23,25 @@ export default class KgoDataCount extends SfdxCommand {
     }
     if (!_.isEmpty(tmpArray)) {
       result[strType] = tmpArray
-      this.ux.log(strType)
       result[strType] = _.orderBy(result[strType], [i => _.isNil(i?.['count']), 'count', 'name'], ['asc', 'desc', 'asc'])
-      this.ux.table(result[strType], formatTabColomn)
-      this.ux.log('')
+    }
+  }
+
+  public static result: SfdxResult = {
+    tableColumnData: {
+      columns: [
+        {key: 'name', label: 'Name'},
+        {key: 'count', label: 'Count'}
+      ]
+    },
+    display() {
+      for (let elem of Object.keys(this.data)) {
+        if (this.data?.[elem]?.length) {
+          this.ux.log(elem)
+          this.ux.table(this.data[elem], this.tableColumnData)
+          this.ux.log('')
+        }
+      }
     }
   }
 
@@ -97,64 +95,13 @@ export default class KgoDataCount extends SfdxCommand {
 
     const objectRecordCount = await conn.request('/limits/recordCount?sObjects=' + new Array<string>().concat(objectsToCount, mdtToCount, custSetToCount, noDataCount).join()) as unknown as sobjectsResult
 
-    // const objectRecordMap = objectRecordCount['sObjects'].map(obj => {
-    //   let rObj = {}
-    //   rObj[obj.name] = obj
-    //   return rObj
-    // })
     const objectRecordMap = keyBy(objectRecordCount['sObjects'], 'name')
     const result = {'Data Storage': [], 'Metadata type Storage': [], 'Custom Settings Storage': [], 'Not counted Storage': []}
-    // let tmpArray = []
-    // let tmpType = 'Data Storage'
-    // for (let elem of objectsToCount) {
-    //   tmpArray.push({'name': elem, 'count': objectRecordMap?.[elem]?.count})
-    // }
-    // result[tmpType] = tmpArray
-    // this.ux.log('\n', tmpType)
-    // result[tmpType] = _.orderBy(result[tmpType], [i => _.isNil(i?.['count']), 'count', 'name'], ['asc', 'desc', 'asc'])
-    // this.ux.table(result[tmpType], formatTabColomn)
+
     this.treatResultType(result, 'Data Storage', objectsToCount, objectRecordMap)
-
-    // tmpArray = []
-    // tmpType = 'Metadata type Storage'
-    // for (let elem of mdtToCount) {
-    //   tmpArray.push({'name': elem, 'count': objectRecordMap?.[elem]?.count})
-    // }
-    // result[tmpType] = tmpArray
-    // this.ux.log('\n', tmpType)
-    // result[tmpType] = _.orderBy(result[tmpType], [i => _.isNil(i?.['count']), 'count', 'name'], ['asc', 'desc', 'asc'])
-    // this.ux.table(result[tmpType], formatTabColomn)
     this.treatResultType(result, 'Metadata type Storage', mdtToCount, objectRecordMap)
-
-    // tmpArray = []
-    // tmpType = 'Custom Settings Storage'
-    // for (let elem of custSetToCount) {
-    //   tmpArray.push({'name': elem, 'count': objectRecordMap?.[elem]?.count})
-    // }
-    // result[tmpType] = tmpArray
-    // this.ux.log('\n', tmpType)
-    // result[tmpType] = _.orderBy(result[tmpType], [i => _.isNil(i?.['count']), 'count', 'name'], ['asc', 'desc', 'asc'])
-    // this.ux.table(result[tmpType], formatTabColomn)
     this.treatResultType(result, 'Custom Settings Storage', custSetToCount, objectRecordMap)
-
-    // tmpArray = []
-    // tmpType = 'Not counted Storage'
-    // for (let elem of noDataCount) {
-    //   tmpArray.push({'name': elem, 'count': objectRecordMap?.[elem]?.count})
-    // }
-    // result[tmpType] = tmpArray
-    // this.ux.log('\n', tmpType)
-    // result[tmpType] = _.orderBy(result[tmpType], [i => _.isNil(i?.['count']), 'count', 'name'], ['asc', 'desc', 'asc'])
-    // this.ux.table(result[tmpType], formatTabColomn)
     this.treatResultType(result, 'Not counted Storage', noDataCount, objectRecordMap)
-
-    // // this.ux.logJson(objectRecordCount)
-    // this.ux.table(objectRecordCount['sObjects'], {
-    //   columns: [
-    //     {key: 'name', label: 'Name'},
-    //     {key: 'count', label: 'Count'}
-    //   ]
-    // })
 
     return result
   }
